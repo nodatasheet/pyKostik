1ebb5c298b56dba07abae81b3c932efc9cd2e0e3
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2023-06-06 12:41:49 +0800

    lambda to var

--- pyKostik.tab/View.panel/ApplyViewPropertiesToOther.pushbutton/ApplyViewPropertiesToOther_script.py
+++ pyKostik.tab/View.panel/ApplyViewPropertiesToOther.pushbutton/ApplyViewPropertiesToOther_script.py
@@ -3,9 +3,10 @@ from pyrevit import DB, HOST_APP, forms, script
 doc = HOST_APP.doc
 active_view = HOST_APP.active_view
 
+except_active_view = (lambda v: v.Id != active_view.Id)
 views = forms.select_views(
     title='Select Views',
-    filterfunc=lambda v: v.Id != active_view.Id,
+    filterfunc=except_active_view,
     multiple=True,
     button_name='Apply Visibility Properties to Views'
 )  # type: list[DB.View]
